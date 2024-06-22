234c8e58e61f701a01dbd3d32358ec3bb3b617eb
amh23 <ayemyahan23@gmail.com>
2024-06-22 22:35:23 +0630

    SinupPage is added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import LogInPage from "./pages/LogInPage";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path='/login' element={<LogInPage /> } />
+          <Route path="/login" element={<LogInPage /> } />
+          <Route path="/signup" element={<SignUpPage /> } />
         </Routes>
       </div>
     </BrowserRouter>
